8494e7fd21789215d4773e55db1d6dbf57908e58
unknown <zubicaray@gmail.com>
2024-05-08 00:02:31 +0200

    ajout du job fixé = en cours de prod

---
+++ src/main/java/org/tecal/scheduler/JobTypeFixed.java
@@ -0,0 +1,108 @@
+package org.tecal.scheduler;
+
+
+
+public class JobTypeFixed extends JobType {
+
+	JobTypeFixed(int barre, String inname) {
+		super(barre, inname);
+		isFixed=true;
+		// TODO Auto-generated constructor stub
+	}
+	
+void makeSafetyBetweenBridges() {
+		
+		long deb = 0;
+		long fin= 0;
+	
+		//System.out.println("Job "+name);
+		for (int taskID = 0; taskID < mTaskOrdoList.size(); ++taskID) {
+						
+			if(mTaskOrdoList.get(taskID).zoneSecu) {
+				deb= mTaskOrdoList.get(taskID).getFixedStartBDD();
+				
+				if(indexAnod > 0 && taskID-1 == indexAnod) {
+					deb=mTaskOrdoList.get(indexAnod).getFixedEndBDD()-CST.TEMPS_ANO_ENTRE_P1_P2;
+				}
+				
+				if(taskID+1 == indexAnod) {
+					fin= mTaskOrdoList.get(indexAnod).getFixedStartBDD();
+					
+				}else {
+					fin= mTaskOrdoList.get(taskID).getFixedEndBDD();
+				}
+				
+				int taskID2 = taskID+1;
+				while(mTaskOrdoList.get(taskID2).zoneSecu && taskID2 < mTaskOrdoList.size())
+				{
+					taskID2++;
+				}
+				
+				if(taskID2>taskID+1) {
+					if(taskID2 == indexAnod) {
+						fin=mTaskOrdoList.get(indexAnod).getFixedStartBDD()+CST.TEMPS_ANO_ENTRE_P1_P2;
+						
+					}else {
+						fin=mTaskOrdoList.get(taskID2-1).getFixedEndBDD(); 
+					}
+					
+					taskID=taskID2;
+				}
+				mNoOverlapP1P2.add(TecalOrdo.model.newFixedInterval( deb,fin -deb, ""));
+				
+				
+				
+			}
+			
+						
+		}
+		
+	}
+	
+	void simulateBridgesMoves() {
+	
+		
+		long deb = 0;
+		long fin= 0;
+		int bridge=0;
+		@SuppressWarnings("unused")
+		TaskOrdo taskOrdoNext =null;
+		//System.out.println("Job "+name);
+		for (int taskID = 0; taskID < mTaskOrdoList.size(); ++taskID) {
+			
+						
+			
+			TaskOrdo taskOrdo = mTaskOrdoList.get(taskID);		
+			
+			if(taskID != mTaskOrdoList.size()-1) 
+				taskOrdoNext = mTaskOrdoList.get(taskID+1);
+		
+			if(taskID >indexAnod) {
+				bridge=1;								
+			}
+			ListeZone lBridgeMoves=bridgesMoves.get(bridge);
+			
+			if(taskID==0) {
+				deb=taskOrdoNext.getFixedStartBDD()-(taskOrdo.tempsDeplacement+CST.TEMPS_ANO_ENTRE_P1_P2);
+				continue;
+			}
+			
+			
+			if(taskOrdo.isOverlapable || taskID ==indexAnod ||  taskID == mTaskOrdoList.size()-1 ) {
+				fin=taskOrdo.getFixedStartBDD()+CST.TEMPS_MVT_PONT;
+				
+				lBridgeMoves.add(TecalOrdo.model.newFixedInterval(deb, fin-deb ,""));
+				
+				if(taskID != mTaskOrdoList.size()-1)
+					deb=taskOrdoNext.getFixedStartBDD()-(taskOrdo.tempsDeplacement+CST.TEMPS_ANO_ENTRE_P1_P2);
+				
+				
+			}			
+			
+		}
+	}
+	
+	
+
+
+}
